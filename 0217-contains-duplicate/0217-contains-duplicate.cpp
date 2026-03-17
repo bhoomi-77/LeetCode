76bbfa6a190289122308005f55900c459b6b27b1
Bhoomika Yadav <ybhoomika2@gmail.com>
2026-03-17 15:33:07 +0530

    Time: 46 ms (66.52%), Space: 90.8 MB (44.07%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+
+        for(int num : nums) {
+            if(m[num] >= 1) {
+                return true;
+            }
+            m[num]++;
+        }
+
+        return false;
+    }
+};

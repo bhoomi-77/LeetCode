91216d95b5be223b8730daec758b91c4acf55bd6
Bhoomika Yadav <ybhoomika2@gmail.com>
2026-03-27 16:08:18 +0530

    Time: 0 ms (100%), Space: 9.9 MB (23.82%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>m;
+        
+        
+        if(s.size()!=t.size())
+        return false;
+        for(auto x:s){
+            m[x]++;
+        }
+        for(auto x:t){
+            if(--m[x]<0)
+            return false;
+        }
+        return true;
+    }
+};

a3c781eb8f1953613bf61ad646e7de1bc1492b9a
Bhoomika Yadav <ybhoomika2@gmail.com>
2026-06-06 18:53:37 +0530

    Time: 13 ms (30.47%), Space: 7.6 MB (88.09%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1) return n;
+        int last=fib(n-1);
+        int slast=fib(n-2);
+        return last+slast;
+    }
+    int main(){
+        int n;
+       cin>>n;
+       fib(n);
+       return 0;
+    }
+};

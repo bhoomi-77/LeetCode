a07c63f6e32fb34515cdb42f92e63b04b04da29c
Bhoomika Yadav <ybhoomika2@gmail.com>
2026-08-01 15:33:26 +0530

    Time: 0 ms (100%), Space: 10.2 MB (37.87%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
          int n=matrix.size();
-         vector<vector<int>> ans(n, vector<int>(n));
        for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
             swap(matrix[i][j],matrix[j][i]);
